668fd1043bc662c02ab1350054700882aa8f9301
tbagby <bagby.timothy@gmail.com>
2016-03-24 18:55:50 -0400

    Version API works

--- Code/routes/index.js
+++ Code/routes/index.js
@@ -11,48 +11,63 @@ router.get('/', function(req, res, next) {
 router.post('/',function(req,res, next) {
     var term = req.body.term;
     var loc = req.body.location;
-  var Yelp = require('yelp');
-
-  var yelp = new Yelp({
-    consumer_key: 'hJgRm9Q_D4gP23TXRJclCQ',
-    consumer_secret: 'd-lZbjjHFOQ_L406KVy5uUOcgGQ',
-    token: 'vRpKS3MAec5o6_p0QLqeDVDni5xlufS0',
-    token_secret: 'H84Kky-s3iPJs_v-x2PIl4qoQAQ',
-  });
-
-  // See http://www.yelp.com/developers/documentation/v2/search_api
-  yelp.search({ term: term, location: loc, sort: '1'})
-    .then(function (data) {
-        //res.send(data);
-        var s = JSON.stringify(data);
-        var obj = JSON.parse(s);
-        var arrayLen = obj.businesses.length;
-        res.write(term + " near " + loc +":\n");
-        res.write("Name \t Rating \t Total Reviews \t Address \t Categories \t Phone\n");
-        for (var i = 0; i < arrayLen; i++) {
-            var busin = obj.businesses[i];
-            if (busin.location.address != "") {
-                res.write(busin.name + " \t ");
-                res.write(busin.rating + "\\5 \t ");
-                res.write(busin.review_count + " \t ");
-                res.write(busin.location.address + ", ");
-                res.write(busin.location.city + " \t ");
-                for (var j = 0; j < busin.categories.length; j++) {
-                    res.write(busin.categories[j][0]);
-                    if (j < (busin.categories.length - 1)) {
-                        res.write(", ");
-                    }
-                }
-                res.write(" \t ");
-                res.write(busin.display_phone +" \t ");
-                res.write("\n");
-            }
-        }
-        res.end()
-    })
-    .catch(function (err) {
-        res.send(err);
-    });
+    var yelp = initYelp();
+    yelp.search({term: term, location: loc, sort: '1'})
+        .then(function (data) {
+            var s = JSON.stringify(data);
+            var obj = JSON.parse(s);
+            var result = obj.businesses;
+            console.log("Starting");
+            console.log(result.length);
+            //for (var i = 0; i < result.length; i++) {
+                //result[i].mobile_url = res[i].mobile_url.substring(0, result[i].mobile_url.indexOf("?"));
+                //result[i].url = result[i].url.substring(0, result[i].url.indexOf("?"));
+                //console.log("Cleaned URLS");
+                //for (var j = 0; j < result[i].categories.length; j++) {
+                    //result[i].categories[j].splice(0,1);
+                //}
+                //console.log("Cleaned Categories");
+            //}
+            console.log(result[0]);
+            res.render('search', { title: 'NomNom', result: result });
+        })
+        .catch(function (err) {
+            res.render('search', { title: 'NomNom', result: "" });
+        });
 });
+
+function initYelp() {
+    var Yelp = require('yelp');
+
+    var yelp = new Yelp({
+        consumer_key: 'hJgRm9Q_D4gP23TXRJclCQ',
+        consumer_secret: 'd-lZbjjHFOQ_L406KVy5uUOcgGQ',
+        token: 'vRpKS3MAec5o6_p0QLqeDVDni5xlufS0',
+        token_secret: 'H84Kky-s3iPJs_v-x2PIl4qoQAQ',
+    });
+    return yelp;
+}
+
+function searchYelp(term, loc, yelp) {
+    // See http://www.yelp.com/developers/documentation/v2/search_api
+    yelp.search({term: term, location: loc, sort: '1'})
+        .then(function (data) {
+            var s = JSON.stringify(data);
+            var obj = JSON.parse(s);
+            var result = obj['businesses'];
+            for (var i =0; i < result.length; i++) {
+                result[i].mobile_url = res[i].mobile_url.substring(0, result[i].mobile_url.indexOf("?"));
+                result[i].url = result[i].url.substring(0, result[i].url.indexOf("?"));
+                for (var j = 0; j < result[i].categories.length; j++) {
+                    result[i].categories[j].splice(0,1);
+                }
+            }
+            console.log(result[0]);
+            res.render('index', { title: 'NomNom', resultName: "" });
+        })
+        .catch(function (err) {
+            res.render('index', { title: 'NomNom', resultName: "" });
+        });
+}
 
 module.exports = router;
