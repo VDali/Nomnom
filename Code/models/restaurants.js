69eeab3fb9f0c6655f2692f185cf3b41e02b5620
VDali <veenadali@gmail.com>
2016-05-02 14:38:24 -0400

    setting up profiles

--- Code/models/restaurants.js
+++ Code/models/restaurants.js
@@ -5,10 +5,9 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var restSchema = new Schema({
-    term: String,
-    location: String,
-    data: String
-
+    term: String, //search term inputted by user in search field
+    location: String, //location inputted by user
+    data: String //data retrieved from Yelp API call is saved in same format to ensure consistency
 });
 
 module.exports = mongoose.model('restaurant', restSchema); //don't forget to export
